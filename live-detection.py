ba76ebaa9170b3dc7faef1f830d3b3b9d9366230
Hira Tehseen <60264080+hiratehseen@users.noreply.github.com>
2023-07-16 23:33:28 +0500

    project

---
+++ live-detection.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+
+# Load YOLO
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+
+# Load classes
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Set up webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+
+    # Create blob from frame
+    blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+
+    # Set the blob as input to the network
+    net.setInput(blob)
+
+    # Perform forward pass
+    outs = net.forward(net.getUnconnectedOutLayersNames())
+
+    # Get bounding box, confidence, and class labels
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:  # Adjust confidence threshold as needed
+                # Scale the bounding box coordinates
+                center_x = int(detection[0] * frame.shape[1])
+                center_y = int(detection[1] * frame.shape[0])
+                width = int(detection[2] * frame.shape[1])
+                height = int(detection[3] * frame.shape[0])
+
+                # Calculate top-left corner coordinates
+                x = int(center_x - width / 2)
+                y = int(center_y - height / 2)
+
+                boxes.append([x, y, width, height])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    # Apply non-maximum suppression to remove overlapping boxes
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # Display the bounding boxes and class labels
+    if len(indices) > 0:
+        for i in indices.flatten():
+            x, y, width, height = boxes[i]
+            label = classes[class_ids[i]]
+            confidence = confidences[i]
+
+            cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+            cv2.putText(frame, f"{label}: {confidence:.2f}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow("Object Detection", frame)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and close the window
+cap.release()
+cv2.destroyAllWindows()
